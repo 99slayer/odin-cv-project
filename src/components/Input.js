ff449d38dfe457f3ec149783ac749559d7db341d
99slayer <21979862+99slayer@users.noreply.github.com>
2023-04-26 20:36:21 -0600

    - Got a basic input component working.

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,54 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 
 export class Input extends Component {
   constructor(props) {
     super(props);
+    // if this.state.saved is not empty hide input field and button
+    this.state = {
+      input: '',
+      saved: '',
+      editing: true
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.submitInput = this.submitInput.bind(this);
+    this.editInput = this.editInput.bind(this);
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value
+    })
+  }
+
+  submitInput = (e) => {
+    this.setState({
+      saved: this.state.input,
+      editing: false
+    })
+  }
+
+  editInput = (e) => {
+    this.setState({
+      editing: true
+    })
   }
 
   render() {
+    let x = null;
+    let y = 'hidden';
+
+    if (!this.state.editing) {
+      x = 'hidden';
+      y = null;
+    }
+
     return (
-      <div></div>
+      <div id={this.props.inputID}>
+        <input className={x} type={this.props.type} onChange={this.handleChange}></input>
+        <p className={y} onClick={this.editInput}>{this.state.saved}</p>
+        <button id={this.props.inputID + '-submit-btn'} className={x} onClick={this.submitInput}>SUBMIT</button>
+      </div>
     );
   }
 }
