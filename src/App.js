839027a0e51b18bdcb2357f38f8a42259a43c6ca
99slayer <21979862+99slayer@users.noreply.github.com>
2023-05-12 16:51:36 -0600

    - Removed useless files. - Bit of cleanup.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Header } from './components/Header';
 import { Info } from './components/Info';
 import { Experience } from './components/Experience';
 import { Education } from './components/Education';
@@ -13,7 +12,6 @@ class App extends Component {
   render() {
     return (
       <div id='app'>
-        <Header></Header>
         <Info></Info>
         <Experience></Experience>
         <Education></Education>
